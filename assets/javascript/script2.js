690fccb1771617c861dba9eacfa5f98c7765a52a
sshampine <sshampine@gmail.com>
2017-02-21 17:37:01 -0600

    adding files

---
+++ assets/javascript/script2.js
@@ -0,0 +1,56 @@
+var target = Math.floor((Math.random()*120) + 19);
+var total = 0;
+var wins = 0;
+var loss = 0;
+$("#match-number").html(target);
+console.log(target);
+
+var guess=[];
+var gems=["assets/images/diamond.jpg", "assets/images/ruby.jpg", "assets/images/emerald.jpg", "assets/images/sapphire.jpg"];
+var gemNum = [];
+
+
+
+
+for (var i=0; i<gems.length; i++) {
+	gemNum.push(Math.floor(Math.random()*12)+1);
+}
+console.log(gemNum);
+ 
+for (var i=0; i < gems.length; i++){
+
+var gem = $("<img>");
+gem.addClass("crystal-images");
+gem.attr("src", gems[i]);
+gem.attr("gemvalues", gemNum[i]);
+$("#crystals").append(gem); 
+};
+
+
+function gamePlay (){
+$(".crystal-images").on("click", function() {
+
+var value = ($(this).attr("gemvalues"));
+
+
+value = parseInt(value); 
+total += value;
+
+
+
+$("#total-area").html(total);
+
+if (total === target) {
+	wins ++;
+	$("#notice").html("Winner!")
+	$("#win").html(wins);
+	
+}
+else if(total > target) {
+	loss ++;
+	$("#loss").html(loss);
+}
+});
+}
+
+gamePlay();
